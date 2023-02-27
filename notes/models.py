cba793067be67205dfecd69df908f03c48de9e06
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2023-02-27 16:13:57 +0200

    lesson 43

--- notes/models.py
+++ notes/models.py
@@ -2,14 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
 class Note(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     upd_date = models.DateTimeField(auto_now=True)
     # reminder field can be left empty if no reminder is needed for the note
     reminder = models.DateTimeField(null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='notes', null=True, blank=True)
 
     def __str__(self):
         return self.title
