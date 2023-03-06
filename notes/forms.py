f24edd147cdb31977ca80900eea12107384fe5f5
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2023-03-06 14:56:10 +0200

    user-auth

--- notes/forms.py
+++ notes/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Note, Category
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
 
 class NoteForm(forms.Form):
@@ -11,27 +26,10 @@ class NoteForm(forms.Form):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
     reminder = forms.DateTimeField(required=False, widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'POST'
-
-        self.helper.layout = Layout(
-            'title',
-            'text',
-            'category',
-            'reminder',
-            Submit('submit', 'Save', css_class='btn btn-primary')
-        )
-
-
-
-    # first().author is a temporary solution
-    def save(self, author=Note.objects.first().author):
+    def save(self):
         note = Note()
         note.title = self.cleaned_data['title']
         note.text = self.cleaned_data['text']
         note.category = self.cleaned_data['category']
         note.reminder = self.cleaned_data['reminder']
-        note.author = author
         return note
